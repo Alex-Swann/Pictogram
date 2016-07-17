bdfefbf853e5f9862ac7f92cf23db9f7e241c95c
Alex-Swann <alex_swann@hotmail.co.uk>
2016-07-17 23:34:25 +0100

    testing root route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => redirect('/sessions/index')
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
+  get "/sessions/index" => "sessions#index"
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
